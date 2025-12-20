429b6ef13f194eab068d820ffa2a3dcbbed119a3
Maksum Nemchenko <maksnemch2004@gmail.com>
2025-12-20 14:47:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const overFlowMenu = document.querySelector('.icon--menu');
+const overFlowMenuClose = document.querySelector('.icon--close');
+
+overFlowMenu.addEventListener('click', () => {
+  page.classList.add('page__menu--open');
+});
+
+overFlowMenuClose.addEventListener('click', () => {
+  page.classList.remove('page__menu--open');
+});
